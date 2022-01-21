1154300806af0af21d511f13f1f168a64c400e8c
Kevin Shibuya <kevinshibuya@hotmail.com>
2022-01-21 15:20:19 -0300

    creating react component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,36 @@
+const repositoryName = 'unform2';
+
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            Access repository
+          </a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            Access repository
+          </a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            Access repository
+          </a>
+        </li>
+      </ul>
+    </section>
+  )
+}
