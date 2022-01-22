73f7c4c71510ced098312c3f291ef06ddf16a1ad
Kevin Shibuya <kevinshibuya@hotmail.com>
2022-01-22 00:37:03 -0300

    using useEffect hook and fetching github api

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,9 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
+
+// https://api.github.com/users/kevinshibuya/repos
 
 const repository = {
   name: 'unform',
@@ -9,6 +12,15 @@ const repository = {
 }
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  // dont forget the second parameter!!!
+  useEffect(() => {
+    fetch('https://api.github.com/users/kevinshibuya/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
