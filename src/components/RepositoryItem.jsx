f7122d0c98e542f1ba08792cf2b34d43f9da0de7
Kevin Shibuya <kevinshibuya@hotmail.com>
2022-01-22 00:50:32 -0300

    consuming api data

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,10 +1,10 @@
 export function RepositoryItem(props) {
   return (
     <li>
-      <strong>{props.repository?.name ?? 'default'}</strong>
+      <strong>{props.repository?.full_name ?? 'default'}</strong>
       <p>{props.repository?.description ?? 'default'}</p>
 
-      <a href={props.repository?.link ?? 'default'}>
+      <a href={props.repository?.html_url ?? 'default'}>
         Access repository
       </a>
     </li>
